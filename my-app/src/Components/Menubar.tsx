d2bfa71aa59544505c995e4f7e8a3f476ab7746c
Jancy Daniel <jancyjaya12@gmail.com>
2024-07-03 14:13:54 +0530

    feat: Add menubar

--- my-app/src/Components/Menubar.tsx
+++ my-app/src/Components/Menubar.tsx
@@ -1,14 +1,18 @@
 
+type menuProp ={
+  setMenu:any
+}
 
-const Menubar = () => {
+const Menubar = (props:menuProp) => {
   return (
     <div className="flex shadow-sm h-10 p-2">
-      <h1 className="ml-48">Car</h1>
-      <h1 className="ml-5">Motorcycles</h1>
-      <h1 className="ml-5">House</h1>
-      <h1 className="ml-5">Scooters</h1>
-      <h1 className="ml-5">Bike</h1>
-      <h1 className="ml-5">Apartments</h1>
+      <h1 onClick={()=> props?.setMenu("Shirt")} className="ml-48 cursor-pointer">Shirt</h1>
+      <h1 onClick={()=> props?.setMenu("Jacket")} className="ml-5 cursor-pointer">Jacket</h1>
+      <h1 onClick={()=> props?.setMenu("Phones")} className="ml-5 cursor-pointer">Mobile Phones</h1>
+      <h1 onClick={()=> props?.setMenu("House")} className="ml-5 cursor-pointer">Houses</h1>
+      <h1 onClick={()=> props?.setMenu("Scooters")} className="ml-5 cursor-pointer">Scooters</h1>
+      <h1 onClick={()=> props?.setMenu("Bike")} className="ml-5 cursor-pointer">Bike</h1>
+      <h1 onClick={()=> props?.setMenu("Apartments")} className="ml-5 cursor-pointer">Apartments</h1>
     </div>
   )
 }
