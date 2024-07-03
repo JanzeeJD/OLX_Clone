ebec0c9d8349717bdfd3b9aed19626436c39533f
Jancy Daniel <jancyjaya12@gmail.com>
2024-07-03 13:05:41 +0530

    feat: add home page component

--- my-app/src/Components/Main.tsx
+++ my-app/src/Components/Main.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import Menubar from "./Menubar"
 import Navbar from "./Navbar"
+import Home from "./Home"
 
 
 const main = () => {
@@ -19,6 +20,7 @@ const main = () => {
     <div>
       <Navbar/>
       <Menubar/>
+      <Home products={prod}/>
     </div>
   )
 }
