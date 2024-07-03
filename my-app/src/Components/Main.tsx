f84a7d6549bc8f065fac07b027d401d28d51f420
Jancy Daniel <jancyjaya12@gmail.com>
2024-07-03 12:47:58 +0530

    feat: create Main.tsx for wrap every component

---
+++ my-app/src/Components/Main.tsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+import Menubar from "./Menubar"
+import Navbar from "./Navbar"
+
+
+const main = () => {
+
+  const [prod,setProd] = useState([])
+
+  const getProducts = () =>{
+    fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProd(json))
+  }
+  useEffect(()=>{
+    getProducts()
+  },[])
+  return (
+    <div>
+      <Navbar/>
+      <Menubar/>
+    </div>
+  )
+}
+
+export default main
