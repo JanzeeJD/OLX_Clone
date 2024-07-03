a5d070b5dc269e56f9012619e7c2e7c8005d039b
Jancy Daniel <jancyjaya12@gmail.com>
2024-07-03 13:34:26 +0530

    feat: update details in home page

--- my-app/src/Components/Home.tsx
+++ my-app/src/Components/Home.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 type productsProp ={
   products:any
@@ -7,14 +8,14 @@ const Home = (props:productsProp) => {
   return (
     <div className="grid grid-cols-4 p-5">
       {props?.products?.map((data:any)=>{
-        return <div className="border border-spacing-1 p-2 ml-3 mt-3">
+        return <Link to="/details" state={{data:data}}> <div className="border border-spacing-1 p-2 ml-3 mt-3">
          <img src={data?.image} className="w-60 h-48" />
          <h1 className="font-bold text-xl">$ {data?.price} </h1>
          <h1> {data?.title} </h1>
          <h1> {data?.category} </h1>
-        </div>
+        </div></Link>
       })}
-    </div>
+    </div> 
   )
 }
 
