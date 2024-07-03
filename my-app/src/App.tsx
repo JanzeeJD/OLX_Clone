197e0a53742d730e40db86714de56b50c98a6913
Jancy Daniel <jancyjaya12@gmail.com>
2024-07-03 13:35:07 +0530

    feat: add route path for detail page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Main from './Components/Main'
+import Details from './Components/Details';
 
 const App = () =>{
 return(
   <div>
     <Routes>
       <Route path='/' element={<Main/>} />
+      <Route path='/details' element={<Details/>} />
     </Routes>
   </div>
 )
